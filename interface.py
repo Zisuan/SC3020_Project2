244a08e8cffdcbb8ea347e3a9fb3531666e192a5
Zisuan <c210128@e.ntu.edu.sg>
2024-04-19 22:19:50 +0800

    Added Advanced Styling with Responsive Design

--- interface.py
+++ interface.py
@@ -11,9 +11,9 @@ class AppGUI:
 
         # SQL Query Input label and area
         self.label_input = tk.Label(master, text="Enter your SQL query:")
-        self.label_input.pack(pady=5)
+        self.label_input.pack(fill=tk.X, padx=75, pady=5)  
         self.query_input = scrolledtext.ScrolledText(master, undo=True, height=10)
-        self.query_input.pack(pady=5)
+        self.query_input.pack(fill=tk.BOTH, expand=True, padx=75, pady=5) 
 
         # Submit Button
         self.submit_button = tk.Button(master, text="Analyze Query", command=self.on_submit)
@@ -21,9 +21,12 @@ class AppGUI:
 
         # Explanation Display Area label and area
         self.label_explanation = tk.Label(master, text="QEP Cost Analysis:")
-        self.label_explanation.pack(pady=5)
-        self.explanation_display = scrolledtext.ScrolledText(master, state='disabled', height=15)
-        self.explanation_display.pack(pady=5)
+        self.label_explanation.pack(fill=tk.X, padx=75, pady=5)
+        self.explanation_display = scrolledtext.ScrolledText(master, state='disabled', height=15, wrap='none')
+        self.explanation_display.pack(fill=tk.BOTH, expand=True, padx=75, pady=(5, 0))
+        self.h_scrollbar = tk.Scrollbar(master, orient='horizontal', command=self.explanation_display.xview)
+        self.h_scrollbar.pack(fill='x', padx=75, pady=(0, 20))
+        self.explanation_display['xscrollcommand'] = self.h_scrollbar.set
 
     def on_submit(self):
         query = self.query_input.get("1.0", tk.END).strip()
